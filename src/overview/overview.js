5bd8213c13f7f5c173b3102e613f76d22321515b
Thorben von Rekowski <rekowski.thorben@gmail.com>
2019-11-04 11:22:25 +0100

    WIP

--- src/overview/overview.js
+++ src/overview/overview.js
@@ -7,15 +7,31 @@ class Overview {
 
     constructor(app){
         this.app = app;
-    }
+        this.exp = [{
+          "name"        : "Test 1",
+          "description" : "Das ist eine Bechreibung"
+      },{
+          "name"        : "Test 2",
+          "description" : "Das ist auch eine Beschreibung"
+      }]
+    };
 
     onShow() {
         //create Container from imported HTML
         let container = document.createElement("div");
         container.innerHTML = overview.trim();
-
         // Anzuzeigende HTML-Elemente ermitteln
-        let section = container.querySelector("#overview").cloneNode(true);
+        let section = container.querySelector("#overview").cloneNode(true).innerHTML;
+
+        for (let expKey in this.exp) {
+            let dummy = document.createElement("div");
+            dummy.innerHTML = section;
+
+
+            // replace Placeholders in the html template with actual values
+            section.innerHTML.replace("$$NAME$$", expKey.name);
+            section.innerHTML.replace("$$DESCRIPTION$$", expKey.description);
+        }
         let content = {
             className: "overview",
             main: section.querySelectorAll("main > *"),
