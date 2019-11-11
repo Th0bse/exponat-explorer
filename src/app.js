508f223fc2205950d16da570b7aa8fe517b9e8a9
Thorben von Rekowski <rekowski.thorben@gmail.com>
2019-11-11 15:43:25 +0100

    Deleted useless detail section

--- src/app.js
+++ src/app.js
@@ -8,61 +8,59 @@ import Erstellen from "./erstellen/erstellen.js";
 import Navigo from "navigo/lib/navigo.js";
 import Overview from "./overview/overview.js";
 import Startview from "./startview/startview.js";
-import Detail from "./detail/detail.js";
+
+/**
+ * Hauptklasse der Anwendung. Kümmert sich darum, die Anwendung auszuführen
+ * und die angeforderten Bildschirmseiten anzuzeigen.
+ */
+class App {
+
+
     /**
-     * Hauptklasse der Anwendung. Kümmert sich darum, die Anwendung auszuführen
-     * und die angeforderten Bildschirmseiten anzuzeigen.
+     * Konstruktor.
      */
-    class App {
-
-
-        /**
-         * Konstruktor.
-         */
-        constructor() {
-            this.exponate = [];
-            this._title = "My App";
-            this._currentView = null;
+    constructor() {
+        this.exponate = [];
+        this._title = "My App";
+        this._currentView = null;
 
         this._router = new Navigo(null, true);
         this._currentUrl = "";
 
         this._router.on({
-            "detail/display/:id":   params => this.showDetail(params.id, "display"),
-            "detail/new":           () => this.showDetail("", "new"),
-            "start":            () => this.showStartView(),
-            "steckbrief":            () => this.showSteckbrief(),
-            "overview":             () => this.showOverview(),
-            "erstellen":             ()=> this.showErstellen(),
-            "*":                    () => this.showOverview(),
+            "start": () => this.showStartView(),
+            "steckbrief": () => this.showSteckbrief(),
+            "overview": () => this.showOverview(),
+            "erstellen": () => this.showErstellen(),
+            "*": () => this.showOverview(),
         });
 
         this._router.hooks({
-        after: (params) => {
-                // Navigation durchführen, daher die neue URL merken
-                this._currentUrl = this._router.lastRouteResolved().url;
+                after: (params) => {
+                    // Navigation durchführen, daher die neue URL merken
+                    this._currentUrl = this._router.lastRouteResolved().url;
                 }
             }
         );
-        }
+    }
 
-        /**
-         * Ab hier beginnt die Anwendung zu laufen.
-         */
-        start() {
-            console.log("Die Klasse App sagt Hallo!");
-            this._router.resolve();
-            this.einlesen();
-        }
+    /**
+     * Ab hier beginnt die Anwendung zu laufen.
+     */
+    start() {
+        console.log("Die Klasse App sagt Hallo!");
+        this._router.resolve();
+        this.einlesen();
+    }
 
-        _switchVisibleView(view) {
-            // Alles klar, aktuelle View nun wechseln
-            document.title = `${this._title} – ${view.title}`;
+    _switchVisibleView(view) {
+        // Alles klar, aktuelle View nun wechseln
+        document.title = `${this._title} – ${view.title}`;
 
-            this._currentView = view;
-            this._switchVisibleContent(view.onShow());
-            return true;
-        }
+        this._currentView = view;
+        this._switchVisibleContent(view.onShow());
+        return true;
+    }
 
     _switchVisibleContent(content) {
         // <header> und <main> des HTML-Grundgerüsts ermitteln
@@ -89,42 +87,41 @@ import Detail from "./detail/detail.js";
         }
     }
 
-    einlesen(){
-      this.addExponat("./img/exponat_bilder/exponat_katzen.jpg","Wildkatzen","Manfred Uwe","Schwarzwald","25.08.2004","01.01.2012-01.01.2020","Willi-Hellermann-Museum");
-      this.addExponat("./img/exponat_bilder/exponat_kopf.jpg","Maya Kopf","-","Amerika","1055","02.10.2017-01.08.2025","Montopolinten Museum of Art");
-      this.addExponat("./img/exponat_bilder/exponat_spiegel.jpg","Antiker Spiegel","Otto Wingler","Schwarzwald","1896","19.04.2005-01.01.2030","Kultur-Museum Karlsruhe");
-      this.addExponat("./img/exponat_bilder/exponat_stoffbox.jpg","Stoffschatulle","-","USA","24.03.1790","02.06.2002-20.02.2022","Louvre");
-      this.addExponat("./img/exponat_bilder/exponat_toterpanda.jpg","Toter Panda","Margit Czeniz","Sydeny","18.05.2000","01.01.2009-01.01.3000","Naturkunde-Museum Heidelberg");
-      this.addExponat("./img/exponat_bilder/exponat_windraad.jpg","Windraad Herkules","Harald Schütz","München","14.03.1968","01.01.2012-01.01.2020","Deutsches Museum");
+    einlesen() {
+        this.addExponat("./img/exponat_bilder/exponat_katzen.jpg", "Wildkatzen", "Manfred Uwe", "Schwarzwald", "25.08.2004", "01.01.2012-01.01.2020", "Willi-Hellermann-Museum");
+        this.addExponat("./img/exponat_bilder/exponat_kopf.jpg", "Maya Kopf", "-", "Amerika", "1055", "02.10.2017-01.08.2025", "Montopolinten Museum of Art");
+        this.addExponat("./img/exponat_bilder/exponat_spiegel.jpg", "Antiker Spiegel", "Otto Wingler", "Schwarzwald", "1896", "19.04.2005-01.01.2030", "Kultur-Museum Karlsruhe");
+        this.addExponat("./img/exponat_bilder/exponat_stoffbox.jpg", "Stoffschatulle", "-", "USA", "24.03.1790", "02.06.2002-20.02.2022", "Louvre");
+        this.addExponat("./img/exponat_bilder/exponat_toterpanda.jpg", "Toter Panda", "Margit Czeniz", "Sydeny", "18.05.2000", "01.01.2009-01.01.3000", "Naturkunde-Museum Heidelberg");
+        this.addExponat("./img/exponat_bilder/exponat_windraad.jpg", "Windraad Herkules", "Harald Schütz", "München", "14.03.1968", "01.01.2012-01.01.2020", "Deutsches Museum");
 //    DEBUG:
-      console.log(this.exponate);
-      this.removeExponat("Wildkatzen");
-      console.log(this.exponate);
+        console.log(this.exponate);
+        this.removeExponat("Wildkatzen");
+        console.log(this.exponate);
     }
 
 
+    addExponat(link, titel, kuenster, ort, erstelldatum, ausstellungszeitraum, ausstellungsort) {
+        this.exponate.push({
+            link: link,
+            titel: titel,
+            kuenstler: kuenster,
+            ort: ort,
+            erstelldatum: erstelldatum,
+            ausstellungszeitraum: ausstellungszeitraum,
+            ausstellungsort: ausstellungsort,
 
-    addExponat(link,titel,kuenster,ort,erstelldatum,ausstellungszeitraum,ausstellungsort){
-      this.exponate.push({
-        link:link,
-        titel: titel,
-        kuenstler: kuenster,
-        ort: ort,
-        erstelldatum: erstelldatum,
-        ausstellungszeitraum: ausstellungszeitraum,
-        ausstellungsort: ausstellungsort,
-
-      });
+        });
 
 
     }
 
-    removeExponat(name){
-      for(var i=0;i<this.exponate.length;i++){
-        if (this.exponate[i].titel==name) {
-          delete this.exponate[i];
+    removeExponat(name) {
+        for (var i = 0; i < this.exponate.length; i++) {
+            if (this.exponate[i].titel == name) {
+                delete this.exponate[i];
+            }
         }
-      }
 
 
     }
@@ -133,6 +130,7 @@ import Detail from "./detail/detail.js";
         let view = new Overview(this);
         this._switchVisibleView(view);
     }
+
     showErstellen() {
         let view = new Erstellen(this);
         this._switchVisibleView(view);
@@ -147,12 +145,6 @@ import Detail from "./detail/detail.js";
         let view = new Steckbrief(this);
         this._switchVisibleView(view);
     }
+}
 
-
-    showDetail(id, mode){
-        let view = new Detail(this, id, mode);
-        this._switchVisibleView(view);
-    }
-    }
-
-    export default App;
+export default App;
